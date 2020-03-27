bc913f12e36a98ec18348688f131e44e00e4ddb3
baoruizhe <814666905@qq.com>
2020-03-27 14:45:09 +0800

    修复了bug....

--- src/main/java/cn/stylefeng/guns/modular/api/controller/ApiAuthTest.java
+++ src/main/java/cn/stylefeng/guns/modular/api/controller/ApiAuthTest.java
@@ -28,6 +28,7 @@ public class ApiAuthTest extends BaseController {
         }
 
         for (int i = 0; i < 10; i++) {
+            System.out.println("修复bug");
             System.out.println(redisUtil.get(i+""));
         }
         return SUCCESS_TIP;
